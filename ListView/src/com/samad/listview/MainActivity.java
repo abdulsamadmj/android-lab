3f2ddd09923cebdbdb364d614c85b2c0815f0bd3
Abdul Samad M J <samadmj4@gmail.com>
2022-03-02 11:50:22 +0530

    imageview and listview added

---
+++ ListView/src/com/samad/listview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.samad.listview;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+	
+	ListView list;
+	String[] listArray= {"Java","Kotlin","XML","Flutter","React","Angular","Vue","Xamarin"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list=(ListView)findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,
+        		android.R.layout.simple_spinner_dropdown_item,listArray);
+        list.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
